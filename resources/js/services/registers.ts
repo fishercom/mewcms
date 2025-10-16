86d1e2efe79d3209ca4b1cb76983614289091178
Fischer Tirado <fishdev@gmail.com>
2025-10-15 22:27:11 -0500

    refactor: Extract objectToFormData to a utility file

--- resources/js/services/registers.ts
+++ resources/js/services/registers.ts
@@ -1,37 +1,13 @@
 
 import { router } from '@inertiajs/react';
 import { Page } from '@inertiajs/core';
+import objectToFormData from '@/lib/form-data';
 
 interface Callbacks {
     onSuccess?: (page: Page) => void;
     onError?: (errors: Record<string, string>) => void;
     onBefore?: () => boolean;
     onFinish?: () => void;
-}
-
-function objectToFormData(obj: Record<string, unknown>, form?: FormData, namespace?: string): FormData {
-    const fd = form || new FormData();
-    let formKey: string;
-
-    for (const property in obj) {
-            if (Object.prototype.hasOwnProperty.call(obj, property)) {
-            if (namespace) {
-                formKey = namespace + '[' + property + ']';
-            } else {
-                formKey = property;
-            }
-
-            // if the property is an object, but not a File, use recursivity.
-            if (typeof obj[property] === 'object' && !(obj[property] instanceof File)) {
-                objectToFormData(obj[property] as Record<string, unknown>, fd, formKey);
-            } else {
-                // if it's a string or a File object
-                fd.append(formKey, obj[property] as string | Blob);
-            }
-        }
-    }
-
-    return fd;
 }
 
 export const getRegisters = (query: Record<string, unknown>) => {
